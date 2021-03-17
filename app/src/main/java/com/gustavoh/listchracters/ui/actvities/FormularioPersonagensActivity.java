bf79e17878ebd1987e46bb5f8c81387e01f59da1
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-03-17 19:25:46 -0300

    FormularioCriado

---
+++ app/src/main/java/com/gustavoh/listchracters/ui/actvities/FormularioPersonagensActivity.java
@@ -0,0 +1,16 @@
+package com.gustavoh.listchracters.ui.actvities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gustavoh.listchracters.R;
+
+public class FormularioPersonagensActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_personagens);
+    }
+}
--- app/src/main/java/com/gustavoh/listchracters/ui/actvities/ListaPersonagemActivity.java
+++ app/src/main/java/com/gustavoh/listchracters/ui/actvities/ListaPersonagemActivity.java
@@ -1,23 +1,23 @@
-package com.gustavoh.listchracters;
+package com.gustavoh.listchracters.ui.actvities;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.sql.Array;
+import com.gustavoh.listchracters.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaPersonagemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acticity_main);
+        setContentView(R.layout.activity_lista_personagem);
 
         List<String> personagens = new ArrayList<>(Arrays.asList("Alex","Ryu","ken","Guile"));//cria a lista com os nomes
 
